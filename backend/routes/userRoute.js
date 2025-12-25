1f5a7092e53c8032f653689c90539ec724aa34f2
Yatendra Kumar <yatendrakumar18113@gmail.com>
2025-12-25 23:47:17 +0530

    feat: add verify OTP route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, verification, loginUser, logoutUser, forgotPassword } from "../controllers/userController.js";
+import { registerUser, verification, loginUser, logoutUser, forgotPassword, verifyOTP } from "../controllers/userController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 // import { verification } from "../controllers/userController.js";
 // import { loginUser } from "../controllers/userController.js";
@@ -11,4 +11,5 @@ router.post('/verify-email', verification)
 router.post('/login', loginUser)
 router.post('/logout', isAuthenticated, logoutUser)
 router.post('/forgot-password', forgotPassword)
+router.post('/verify-otp', verifyOTP)
 export default router;
